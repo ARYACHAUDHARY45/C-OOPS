ca9d076200daa79ea9ff35366ee9ad18b0101775
ARYACHAUDHARY45 <azaryvasu540@gmail.com>
2025-08-04 22:12:53 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main (){
+    cout << "Hello World!";
+    //this is syntax of comments.
+    // This is a single line comment.
+    /* This is a multi-line comment.    
+       It can span multiple lines. */   
+    return 0;
+}
